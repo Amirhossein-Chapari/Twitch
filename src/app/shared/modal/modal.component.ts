7a6f009363bfded1d84c592498231556709e760b
Amirhossein-Chapari <amirhossein.chapari80@gmail.com>
2025-03-17 00:15:14 +0330

    tabs cmp completed

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, ElementRef, inject, Input, OnInit } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
@@ -7,10 +7,18 @@ import { ModalService } from '../../services/modal.service';
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+
   modalService = inject(ModalService)
+  elementRef = inject(ElementRef)
+
+  ngOnInit(): void {
+    document.body.appendChild(this.elementRef.nativeElement)
+  }
+  
   @Input() modalId = '';
   closeModal() {
     this.modalService.toggleModal(this.modalId)
   }
+  
 }
