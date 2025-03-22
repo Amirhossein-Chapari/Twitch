095a91adbd6306c16580f4d4f711c1dcf6b59250
Amirhossein-Chapari <amirhossein.chapari80@gmail.com>
2025-03-22 11:34:11 +0330

    register updated

--- src/app/components/user/auth-modal/auth-modal.component.ts
+++ src/app/components/user/auth-modal/auth-modal.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { SharedModule } from "../../../shared/shared.module";
 import { ModalService } from '../../../services/modal.service';
+import { LoginComponent } from "../login/login.component";
+import { RegisterComponent } from "../register/register.component";
 
 
 @Component({
   selector: 'app-auth-modal',
-  imports: [SharedModule],
+  imports: [SharedModule, LoginComponent, RegisterComponent],
   templateUrl: './auth-modal.component.html',
   styleUrl: './auth-modal.component.scss'
 })
