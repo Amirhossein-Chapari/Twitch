ec273d8d65b6962dfef59c495def5c3d6ff0590b
Amirhossein-Chapari <amirhossein.chapari80@gmail.com>
2025-03-24 18:31:20 +0330

    register validators & cmp completed

---
+++ src/app/components/user/register/register.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ValidationErrors, AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../../shared/shared.module';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule, SharedModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+
+  inSubmission = false;
+
+  registerForm = new FormGroup({
+    name: new FormControl<string | null>(null, [Validators.required, Validators.minLength(3)]),
+    email: new FormControl<string | null>(null, [
+      Validators.required,
+      Validators.email,
+      Validators.pattern(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/),
+    ]),
+    age: new FormControl<number | null>(null, [Validators.required, Validators.min(3)]),
+    phone: new FormControl<string | null>(null, [
+      Validators.required,
+      Validators.pattern(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/),
+    ]),
+    password: new FormControl<string | null>(null, [
+      Validators.required,
+      Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/),
+    ]),
+    confirmPassword: new FormControl<string | null>(null, [Validators.required]),
+  }, { validators: RegisterComponent.passwordMatchValidator });
+
+  static passwordMatchValidator(form: AbstractControl): ValidationErrors | null {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password && confirmPassword && password !== confirmPassword ? { mismatch: true } : null;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabComponent } from './tab/tab.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { InputsComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 
 @NgModule({
-  declarations: [ModalComponent, TabComponent, TabsContainerComponent],
+  declarations: [ModalComponent, TabComponent, TabsContainerComponent, InputsComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule
   ],
-  exports: [ModalComponent, TabComponent, TabsContainerComponent]
+  exports: [ModalComponent, TabComponent, TabsContainerComponent, InputsComponent]
 })
 export class SharedModule { }
